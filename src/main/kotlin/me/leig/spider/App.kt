4b9102ccc9963ec956afa92e8e18e25264ec11ae
leigme@gmail.com <leigme@gmail.com>
2017-12-04 00:37:15 +0800

    add imageDownload

--- src/main/kotlin/me/leig/spider/App.kt
+++ src/main/kotlin/me/leig/spider/App.kt
@@ -1,8 +1,10 @@
 package me.leig.spider
 
+import me.leig.spider.comm.makeImage
 import me.leig.spider.main.Spider
 
 fun main(args: Array<String>) {
     println("app start...")
-    val spider = Spider().analysisUrl("http://home.meishichina.com/show-top-type-recipe.html")
+    val spider = Spider().analysisUrl("")
+    makeImage("http://i3.meishichina.com/attachment/recipe/2017/11/29/20171129151194979042613.jpg@!c320", "222.jpg")
 }
--- src/main/kotlin/me/leig/spider/comm/tools.kt
+++ src/main/kotlin/me/leig/spider/comm/tools.kt
@@ -1,2 +1,41 @@
 package me.leig.spider.comm
 
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.net.URL
+
+
+fun makeImage(url: String, fileName: String) {
+    try {
+
+        val inFile = URL(url).openStream()
+
+//        val metadata = JpegMetadataReader.readMetadata(inFile)
+//
+//        val exifs = metadata.directories
+//
+//        for (exif in exifs) {
+//            val tags = exif.tags.iterator()
+//            while (tags.hasNext()) {
+//                println(tags.next())
+//            }
+//        }
+
+        val bis = BufferedInputStream(inFile)
+        val file = File(fileName)
+        val bos = BufferedOutputStream(FileOutputStream(file))
+        var data = ByteArray(1024)
+        var length = bis.read(data)
+        while (-1 != length) {
+            bos.write(data, 0, length)
+            length = bis.read(data)
+        }
+        println("正在执行下载任务：当前正在下载图片[$fileName]")
+        bis.close()
+        bos.close()
+    } catch (e: Exception) {
+        throw e
+    }
+}
