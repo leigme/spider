085739f0af7c15a8050e6a112f537875b43250fa
leigme <leigme@gmail.com>
2017-12-26 08:23:23 +0800

    修改解析逻辑

--- src/main/kotlin/me/leig/spider/App.kt
+++ src/main/kotlin/me/leig/spider/App.kt
@@ -1,14 +1,8 @@
 package me.leig.spider
 
-import me.leig.spider.comm.makeImage
 import me.leig.spider.main.Spider
 
 fun main(args: Array<String>) {
     println("app start...")
-    val urls = Spider().analysisUrl("http://www.jiepaiuu.com/forum-2-1.html")
-    for (url in urls) {
-        makeImage(Spider().analysisUrlToBigImage(url), "temps\\${Spider().analysisUrlToBigImage(url).split("/")[Spider().analysisUrlToBigImage(url).split("/").size - 1]}")
-    }
-
-
+    Spider().analysisUrl("http://www.jiepaiuu.com/forum-2-1.html")
 }
--- src/main/kotlin/me/leig/spider/main/Spider.kt
+++ src/main/kotlin/me/leig/spider/main/Spider.kt
@@ -2,19 +2,42 @@ package me.leig.spider.main
 
 import me.leig.spider.comm.makeImage
 import org.jsoup.Jsoup
+import java.io.File
 
 class Spider {
 
-    fun analysisUrl(url: String): MutableList<String> {
+    fun analysisUrl(url: String) {
         try {
-            val doc = Jsoup.connect(url).get()
-            val elements = doc.select("[class=ml mlt mtw cl]").select("[class=c cl]").select("a").select("img")
+            var doc = Jsoup.connect(url).get()
+            var elements = doc.select("[class=ml mlt mtw cl]").select("[class=c cl]").select("a").select("img")
             val urls = mutableListOf<String>()
             elements
                     .map { it.attr("src") }
                     .mapTo(urls) { "http://www.jiepaiuu.com/thread-${it.split("/")[it.split("/").size - 1].split(".")[0]}-1-1.html" }
 
-            return urls
+            for (url in urls) {
+                makeImage(analysisUrlToBigImage(url), StringBuffer("temps").append(File.separatorChar).append(Spider().analysisUrlToBigImage(url).split("/")[Spider().analysisUrlToBigImage(url).split("/").size - 1]).toString())
+            }
+
+            val strNum = doc.select("[class=last]").text().split(" ")
+
+            val pageNum = strNum[strNum.size - 1].trim()
+
+            for (i in 1 until pageNum.toInt()) {
+
+                val newUrl = url.split("1.")
+
+                doc = Jsoup.connect(newUrl[0] + "${i}." + newUrl[1]).get()
+                elements = doc.select("[class=ml mlt mtw cl]").select("[class=c cl]").select("a").select("img")
+
+                val urls = mutableListOf<String>()
+                elements
+                        .map { it.attr("src") }
+                        .mapTo(urls) { "http://www.jiepaiuu.com/thread-${it.split("/")[it.split("/").size - 1].split(".")[0]}-1-1.html" }
+                for (url in urls) {
+                    makeImage(analysisUrlToBigImage(url), StringBuffer("temps").append(File.separatorChar).append(Spider().analysisUrlToBigImage(url).split("/")[Spider().analysisUrlToBigImage(url).split("/").size - 1]).toString())
+                }
+            }
 
         } catch (e: Exception) {
             throw e
